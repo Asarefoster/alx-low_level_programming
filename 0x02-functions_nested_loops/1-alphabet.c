e88a36fa0aa1dd894f266d29e17f3012d790359e
Asare foster <asarefosterinnocent@gmail.com>
2023-06-20 19:54:23 -0700

    1 alphab

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,13 @@
 *Description: function that prints the alphabet, in lowercase
 *Return: always 0
 */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int i;
 
-	for (i = '97'; i <= '122'; i++)
+	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
 	}
 	_putchar('\n');
-	return (0);
 }
