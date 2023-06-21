857dac502bc83561f7acef1e8c73aa21cb48c93b
Asare foster <asarefosterinnocent@gmail.com>
2023-06-20 18:51:17 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*main - entry point
+*Description: function that prints the alphabet, in lowercase
+*Return: always 0
+*/
+void print_alphabet(void);
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n')
+}
