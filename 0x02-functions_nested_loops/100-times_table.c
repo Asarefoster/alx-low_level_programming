a837eed7cd85bf4bea04cf8a5ae7bd4ac98a76b4
Asare foster <asarefosterinnocent@gmail.com>
2023-06-21 15:27:40 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,17 +12,17 @@ void print_times_table(int n);
 
 	if (n >= 0 && n <= 15)
 	{
-	for (num = 0; num <= n; n++)
+	for (num = 0; num <= n; num++)
 	{
 	_putchar('0');
-	for (mult = 1; mult >= n; mult++)
+	for (mult = 1; mult <= n; mult++)
 	{
 	_putchar(',');
 	_putchar(' ');
 	prod = num * mult;
 	if (prod <= 99)
 	_putchar(' ');
-	if (prod <= 0)
+	if (prod <= 9)
 	_putchar(' ');
 	if (prod >= 100)
 	{
