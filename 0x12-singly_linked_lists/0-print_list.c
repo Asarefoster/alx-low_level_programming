ad2d88623d00ece7c108e5d34024eed6320a6674
Asare foster <asarefosterinnocent@gmail.com>
2023-07-28 01:46:30 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,9 +16,10 @@ size_t print_list(const list_t *h)
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			Printf("[%u] %s\n", h->|en, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		s++;
 	}
+
 	return (s);
 }
