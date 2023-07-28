a347e46b1c82a2494029faf70b11a376bc56416e
Asare foster <asarefosterinnocent@gmail.com>
2023-07-28 02:30:26 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,11 +2,11 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
- *
- * Return: the number of nodes printed
- */
+* print_list - prints all the elements of a linked list
+* @h: pointer to the list_t list to print
+*
+* Return: the number of nodes printed
+*/
 
 size_t print_list(const list_t *h)
 {
@@ -21,5 +21,6 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		s++;
 	}
+
 	return (s);
 }
