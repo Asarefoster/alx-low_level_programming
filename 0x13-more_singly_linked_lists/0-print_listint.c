17a9fa9b156fbfcc399a5d3a18e9f89378dbe9bc
Asare foster <asarefosterinnocent@gmail.com>
2023-08-01 16:43:32 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,6 +5,7 @@
   *@h: linked list of type listint_t to print
   *Return: number of nodes
   */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
@@ -13,6 +14,7 @@ size_t print_listint(const listint_t *h)
 	{
 		printf("%d\n", h->n);
 		num++;
+
 		h = h->next;
 	}
 
