5af0723a0b64ad9fcc96f72a503dd3ff6ad94bcc
Asare foster <asarefosterinnocent@gmail.com>
2023-08-12 02:52:05 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -140,7 +140,8 @@ void print_data(unsigned char *e_ident)
 
 void print_version(unsigned char *e_ident)
 {
-	 printf(" Version: %d (current)\n", e_ident[EI_VERSION]);
+	 printf(" Version: %d (current)\n",
+			 e_ident[EI_VERSION]);
 
 	switch (e_ident[EI_VERSION])
 	{
